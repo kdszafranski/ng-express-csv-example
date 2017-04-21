d92b31e729ff598475f239a03debfc897a53e198
Kris Szafranski <kdszafranski@gmail.com>
2017-04-21 15:45:51 -0500

    added in angular csv solution example

--- server/app.js
+++ server/app.js
@@ -1,22 +1,45 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var csv = require('express-csv');
 
 // Serve back static files
 app.use(express.static('./server/public'));
 
-app.use(function(req, res, next) {
-  var thing = "hello";
-  req.message = "hi";
-  console.log('middleware');
-  next(thing);
-});
+app.get('/getcsv', function(req, res) {
+  var mongoObject = {
+    _id: '1234',
+    firstName: 'First',
+    lastName: 'Last'
+  };
 
-app.use(function(newvar, req, res, next) {
-  console.log('passed var: ', newvar);
-  console.log('property on req: ', req.message);
-  next();
-});
+  // create an array from the mongo object so we can use .unshift() later
+  var data = [mongoObject];
+
+  // create a header row from the object's keys/properties
+  var headers = Object.keys(mongoObject);
+
+  // push keys array to the beginning of data array
+  data.unshift(headers);
+
+  console.log('data: ', data);
+
+  res.attachment('testing.csv');   // not really used
+  res.csv(data);
+})
+
+// app.use(function(req, res, next) {
+//   var thing = "hello";
+//   req.message = "hi";
+//   console.log('middleware');
+//   next(thing);
+// });
+//
+// app.use(function(newvar, req, res, next) {
+//   console.log('passed var: ', newvar);
+//   console.log('property on req: ', req.message);
+//   next();
+// });
 
 // Handle index file separately
 app.get('/', function(req, res) {
--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,8 +1,24 @@
-myApp.controller('HomeController', function() {
+myApp.controller('HomeController', function($http) {
   console.log('home controller running');
 
   var self = this;
 
   self.message = "Welcome to the Home View";
+
+  self.download = function() {
+    $http.get('/getcsv').then(function(result) {
+      var blob = new Blob([result.data], { type: result.config.dataType });
+      var windowUrl = (window.URL || window.webkitURL);
+      var downloadUrl = windowUrl.createObjectURL(blob);
+      var anchor = document.createElement("a");
+      anchor.href = downloadUrl;
+      // var fileNamePattern = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/
+      // anchor.download = fileNamePattern.exec(headers['content-disposition'])[1]
+      anchor.download = "distributions.csv";
+      document.body.appendChild(anchor);
+      anchor.click();
+      windowUrl.revokeObjectURL(blob);
+    });
+  }
 
 });
