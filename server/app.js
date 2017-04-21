aa5dc8d853168c7aa4ff13f97cbc61eb7bac9b4c
Kris Szafranski <kdszafranski@gmail.com>
2017-04-21 15:34:26 -0500

    made changes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,19 @@ var path = require('path');
 // Serve back static files
 app.use(express.static('./server/public'));
 
+app.use(function(req, res, next) {
+  var thing = "hello";
+  req.message = "hi";
+  console.log('middleware');
+  next(thing);
+});
+
+app.use(function(newvar, req, res, next) {
+  console.log('passed var: ', newvar);
+  console.log('property on req: ', req.message);
+  next();
+});
+
 // Handle index file separately
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./server/public/views/index.html'));
