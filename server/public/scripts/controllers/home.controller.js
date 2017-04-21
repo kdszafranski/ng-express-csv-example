23325e9222c1fb4976dbc71f290638f27a3124f7
Kris Szafranski <kdszafranski@gmail.com>
2017-04-21 16:09:53 -0500

    documenting client side code

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -7,16 +7,25 @@ myApp.controller('HomeController', function($http) {
 
   self.download = function() {
     $http.get('/getcsv').then(function(result) {
+      // create an object and a new DOM element and then make the browser 'click' on it
+      // https://developer.mozilla.org/en-US/docs/Web/API/Blob
       var blob = new Blob([result.data], { type: result.config.dataType });
       var windowUrl = (window.URL || window.webkitURL);
       var downloadUrl = windowUrl.createObjectURL(blob);
+
+      // create new <a> tag
       var anchor = document.createElement("a");
+      // URL is just our object from above
       anchor.href = downloadUrl;
-      // var fileNamePattern = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/
-      // anchor.download = fileNamePattern.exec(headers['content-disposition'])[1]
-      anchor.download = "distributions.csv";
+      // name the file to download
+      anchor.download = "mydownloadedfile.csv";
+
       document.body.appendChild(anchor);
+
+      // simulate a click event on this a tag
       anchor.click();
+
+      // destroy the created URL Object from above to clean up
       windowUrl.revokeObjectURL(blob);
     });
   }
