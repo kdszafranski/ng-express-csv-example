06dfbf8894b9d9daad3cff71f4f14c796a1bc00f
Kris Szafranski <kdszafranski@gmail.com>
2017-04-24 13:27:22 -0500

    added mongodb integration with mongoose

--- server/app.js
+++ server/app.js
@@ -2,6 +2,28 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var csv = require('express-csv');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// step 1: create the Schema
+var personSchema = new Schema({
+  name: {type: String, required: true},
+  location: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    databaseUri = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    databaseUri = 'mongodb://localhost:27017/sigma';
+}
+
+mongoose.connect(databaseUri);
+
 
 // Serve back static files
 app.use(express.static('./server/public'));
@@ -14,40 +36,42 @@ app.use(express.static('./server/public'));
 // https://github.com/jasunde/christian-cupboard/blob/64a5994f42042c9cd147c20eddcf382507b03bb3/server/routes/distributions.js#L191
 
 app.get('/getcsv', function(req, res) {
-  // this would be your returned find() object 
-  var mongoObject = {
-    _id: '1234',
-    firstName: 'First',
-    lastName: 'Last'
-  };
+  // this would be your returned find() object
 
-  // create an array from the mongo object so we can use .unshift() later
-  var data = [mongoObject];
+  Person.findOne({}, function(err, result) {
+    if(err) {
+      console.log('Get ERR: ', err);
+      res.sendStatus(500);
+    } else {
+      var obj = result.toObject();
+      obj.test = "hello";
+      console.log('person: ', obj);
 
-  // create a header row from the object's keys/properties
-  var headers = Object.keys(mongoObject);
+      // create an array from the mongo object so we can use .unshift() later
+      var data = [obj];
 
-  // push keys array to the beginning of data array
-  data.unshift(headers);
+      // create a header row from the object's keys/properties
+      var headers = Object.keys(obj);
 
-  console.log('data: ', data);
+      // push keys array to the beginning of data array
+      data.unshift(headers);
 
-  res.attachment('testing.csv');   // not really used
-  res.csv(data);
+      console.log('data: ', data);
+
+      res.attachment('testing.csv');   // not really used
+      res.csv(data);
+    }
+  });
+  //
+  // var mongoObject = {
+  //   _id: '1234',
+  //   firstName: 'First',
+  //   lastName: 'Last'
+  // };
+
+
 })
 
-// app.use(function(req, res, next) {
-//   var thing = "hello";
-//   req.message = "hi";
-//   console.log('middleware');
-//   next(thing);
-// });
-//
-// app.use(function(newvar, req, res, next) {
-//   console.log('passed var: ', newvar);
-//   console.log('property on req: ', req.message);
-//   next();
-// });
 
 // Handle index file separately
 app.get('/', function(req, res) {
